8fd393cb73125f7784773884a8e5e99ba7563557
Josephine Njoki Njuguna <josephine.njuguna@student.moringaschool.com>
2026-08-01 15:41:22 +0300

    addded the name and the title

--- src/components/PosterHeading.jsx
+++ src/components/PosterHeading.jsx
@@ -2,8 +2,7 @@ function PosterHeading() {
   return (
     <div className="bg-black text-white px-10 py-8">
       <h1 className="text-6xl font-bold leading-none">
-        Albert <br />
-        Odero
+        Albert Odero
       </h1>
 
       <h2 className="text-3xl mt-6 uppercase tracking-wide">
